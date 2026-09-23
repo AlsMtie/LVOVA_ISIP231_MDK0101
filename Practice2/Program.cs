fa3af7bd0f0f237e90bec10f264e57e9f8bba28c
AlsMtie <133372067+AlsMtie@users.noreply.github.com>
2026-09-23 11:05:04 +0900

    -

--- Practice2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Console.Write("курс доллара:");
-// double curs = double.Parse(Console.ReadLine());
-// Console.Write("сумма в рублях: ");
-// double rub = double.Parse(Console.ReadLine());
-// Console.WriteLine(rub / curs);

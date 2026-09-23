6ebde9504d62e0a122785086387499b55fee77f6
AlsMtie <133372067+AlsMtie@users.noreply.github.com>
2026-09-23 11:05:18 +0900

    -

--- Practice3/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// double sum = 0;
-// for (int i = 1; i <= 7; i++)
-// {
-//     Console.Write($"День {i}: ");
-//     sum += double.Parse(Console.ReadLine());
-// }
-// Console.WriteLine($"Ср темп: {sum / 7}");
